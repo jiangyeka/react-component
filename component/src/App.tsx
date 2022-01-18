cca2324336f17722bff84ffaf8b97bd29cca7e3f
jiangyeka <940495614@qq.com>
2022-01-18 22:13:10 +0800

    修改react版本

--- component/src/App.tsx
+++ component/src/App.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
-import Button,{ButtonType,ButtonSize} from './components/Button/button';
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Button autoFocus onClick={e=>{e.preventDefault();alert(12)}} >Hello</Button>
-        <Button className='fff' disabled>Disabled Button</Button>
-        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
-          Large Primary
-        </Button>
-        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>
-          Small Danger
-        </Button>
-        <Button btnType={ButtonType.Link} href='http://www.baidu.com' target="_blank">
-          Baidu Link
-        </Button>
-        <Button btnType={ButtonType.Link} href='http://www.baidu.com' disabled>
-          Disabled Link
-        </Button>  </header>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }

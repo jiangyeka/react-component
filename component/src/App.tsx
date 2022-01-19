26a610437174fbbf6d0b9f273ad23d90dd5a56c4
jiangyeka <940495614@qq.com>
2022-01-19 13:00:48 +0800

    icon开发

--- component/src/App.tsx
+++ component/src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Button,{ButtonType,ButtonSize} from './components/Button/button';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
+import Icon from './components/Icon/icon';
+library.add(fas)
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+
+      <Icon icon="coffee" theme="danger" size='10x'/>
+
         <Button className='cuntom'>fff</Button>
-      <Button onClick={e=>{e.preventDefault();alert(123)}}> Default Button </Button>
+        <Button onClick={e=>{e.preventDefault();alert(123)}}> Default Button </Button>
         <Button disabled> Disabled Button </Button>
         <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Large Primary </Button>
         <Button btnType={ButtonType.Danger} size={ButtonSize.Small}> Small Danger </Button>

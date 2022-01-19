c09cc7969d706dd5169c9fbb6e27a5e6ca83d349
jiangyeka <940495614@qq.com>
2022-01-19 11:03:00 +0800

    button:测试用例

--- component/src/App.tsx
+++ component/src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button,{ButtonType,ButtonSize} from './components/Button/button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Button className='cuntom'>fff</Button>
+      <Button onClick={e=>{e.preventDefault();alert(123)}}> Default Button </Button>
+        <Button disabled> Disabled Button </Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Large Primary </Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}> Small Danger </Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com" target="_blank"> Link  </Button>
+        <Button btnType={ButtonType.Link} disabled href="http://www.baidu.com"> Disabled Link  </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
